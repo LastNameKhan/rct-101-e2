51e06c73c6a34770bfc36f57312b1783db49e3a7
LastNameKhan <lastnamekhan47@gmail.com>
2022-05-30 17:22:21 +0530

    Sprint 2 evaluation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { Button, ButtonGroup } from "@chakra-ui/react";
 const App = () => {
-  return <div>{/* TODO: Code here */}</div>;
+  return (
+    <div>
+      {/* TODO: Code here */}
+      <Button colorScheme="teal" size="xs">
+        Button
+      </Button>
+    </div>
+  );
 };
 
 export default App;
--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AddProduct = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Button = () => <div />;
-  const Modal = () => <div />;
-  const ModalBody = () => <div />;
-  const Input = () => <div />;
-  const Select = () => <div />;
-  const RadioGroup = () => <div />;
-  const Radio = () => <div />;
+  // const Button = () => <div />;
+  // const Modal = () => <div />;
+  // const ModalBody = () => <div />;
+  // const Input = () => <div />;
+  // const Select = () => <div />;
+  // const RadioGroup = () => <div />;
+  // const Radio = () => <div />;
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const saveInfo = () => {
+    //call api to save this information in backend
+    fetch("http://localhost:8080/products", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        value: newTodo,
+        isCompleted: false,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos([...todos, data]);
+        setNewTodo("");
+      });
+  };
+  useEffect(() => {
+    fetch("http://localhost:8080/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data);
+      });
+  }, []);
 
   return (
     <>
       <Button my={4} data-cy="add-product-button"></Button>
       <Modal>
         <ModalBody pb={6}>
-          <Input data-cy="add-product-title" />
+          <Input
+            value={newTodo}
+            onChange={({ target }) => setNewTodo(target.value)}
+            data-cy="add-product-title"
+          />
           <Select data-cy="add-product-category">
             <option data-cy="add-product-category-shirt"></option>
             <option data-cy="add-product-category-pant"></option>
